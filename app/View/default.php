01a4d4fd0ac0390d6785f5bb848b7a103c5eb5e3
yuichiro12 <talcnrhyme@gmail.com>
2017-10-25 19:40:42 +0900

    assetファイル追加

--- app/View/default.php
+++ app/View/default.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="UTF-8"/>
         <title>BBS</title>
+		<link rel="stylesheet" type="text/css" href="/css/default.css">
+		<script src="/js/default.js"></script>
     </head>
     <body>
 	  @contents
