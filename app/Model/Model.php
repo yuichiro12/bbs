890195610059eb717d43163d6c3d055704f8445f
yuichiro12 <talcnrhyme@gmail.com>
2017-10-30 06:39:44 +0900

    Userモデル追加，モデル処理の一般化

--- app/Model/Model.php
+++ app/Model/Model.php
@@ -5,36 +5,72 @@ namespace app\Model;
 
 class Model
 {
-    protected $db;
-    protected static $model;
-    protected $columns;
+    protected $db = null;
+    protected static $model = '';
+    protected static $columns = [];
 
     public function __construct() {
         $this->initDatabase();
+        date("Y-m-d H:i:s");
     }
 
-    // TODO: 一般化，バリデーション
-    public function save($params) {
-        // $implode(', ', $params);
-        // $values = array_map(function(){}, );
-        
-        $stmt = $this->db->prepare('INSERT INTO ' . static::$model . '(name, body, created_at) VALUES(:name, :body, :created_at)');
-        $stmt->bindValue(':name', $params["name"]);
-        $stmt->bindValue(':body', $params["body"]);
-        $stmt->bindValue(':created_at', date("Y-m-d H:i:s"));
+    public function save($data) {
+        $params = $this->setTime($data);
+        $count = count($params);
+        $columns = implode(', ', array_keys($params));
+        $ph = implode(', ', array_fill(0, $count, '?'));
+
+        $stmt = $this->db->prepare(
+            'INSERT INTO ' . static::$model . "($columns) VALUES($ph)"
+        );
+
+        $i = 1;
+        foreach ($params as $v) {
+            $stmt->bindValue($i, $v);
+            $i++;
+        }
+
         $stmt->execute();
     }
 
-    public function findAll() {
-        $query = 'SELECT * FROM ' . static::$model;
+    public function findAll($column = null, $order = 'ASC') {
+        $sort = $this->getOrder($column, $order);
+        $query = 'SELECT * FROM ' . static::$model . $sort;
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll();
     }
 
+    // バリデーション（マスアサインメント対策）
+    public function validate($data) {
+        $columns = static::$columns;
+        $params = [];
+
+        foreach ($columns as $k => $v) {
+            $params[$k] = isset($data[$k]) ? $data[$k] : $v;
+        }
+        return $params;
+    }
+
+    // DB接続
     protected function initDatabase() {
         $env = json_decode(file_get_contents(__DIR__ . "/../../.env"), true);
         $dsn = "mysql:dbname={$env['dbname']}; host={$env['host']}; charset={$env['charset']}";
         $this->db = new \PDO($dsn, $env['user'], $env['password']);
+    }
+
+    private function setTime($data) {
+        if (isset($data['created_at']) && $data['created_at'] === '') {
+            $data['created_at'] = date("Y-m-d H:i:s");
+        }
+        if (isset($data['updated_at']) && $data['updated_at'] === '') {
+            $data['updated_at'] = date("Y-m-d H:i:s");
+        }
+
+        return $data;
+    }
+
+    private function getOrder($column, $order) {
+        return is_null($column) ? '' : " ORDER BY `$column` $order";
     }
 }
--- app/Model/Posts.php
+++ app/Model/Posts.php
@@ -3,6 +3,13 @@ namespace app\Model;
 
 class Posts extends Model
 {
-    protected $columns = ["id", "name", "body", "user_id", "created_at"];
     protected static $model = 'posts';
+    // マスアサインメント対策
+    // TODO: 型の情報を持たせる
+    protected static $columns = [
+        'user_id' => null,
+        'body' => '',
+        'name' => '',
+        'created_at' => '',
+    ];
 }
