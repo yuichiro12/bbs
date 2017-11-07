082adba1256a12f6be16008f226079cd181dd9dc
yuichiro12 <talcnrhyme@gmail.com>
2017-11-07 22:47:02 +0900

    スマホ対応など細かいレイアウト修正

--- app/View/threads/show.php
+++ app/View/threads/show.php
@@ -9,9 +9,17 @@
 	<span class="uname"><?= h($post['name']) ?></span>
 	<span><?= h($post['created_at']) ?></span>
 	<?php if (isset($_SESSION) && ((int)$post['user_id'] === (int)$_SESSION['user_id'])): ?>
-	  <span><a href="/posts/edit/<?= $post['id'] ?>">編集</a></span>
+	  <span>
+		<a href="/posts/edit/<?= $post['id'] ?>" class="post-edit-link">
+		  <span class="oi oi-pencil" title="編集"></span>edit
+		</a>
+	  </span>
 	  <form action="/posts/delete/<?= $post['id'] ?>" method="post" class="inline-wrapper">
-		<span class="deletePost"><a href="javascript:void(0)">削除</a></span>
+		<span class="deletePost">
+		  <a href="javascript:void(0)" class="post-delete-link">
+			<span class="oi oi-pencil" title="削除"></span>delete
+		  </a>
+		</span>
 		<?= csrf_token() ?>
 	  </form>
 	<?php endif; ?>
@@ -19,21 +27,22 @@
 	  <span> (modified at <?= $post['updated_at'] ?>)</span>
 	<?php endif; ?>
 	<?php if ($post['deleted_flag'] === '1'): ?>
-	  <div>この投稿は削除されました。</div>
+	  <div class="deleted"><i>この投稿は削除されました。</i></div>
 	<?php else: ?>
 	  <div><?= h($post['body']) ?></div>
 	<?php endif; ?>
   </div>
   <br/>
+  <br/>
 <?php endforeach; ?>
 <hr/>
 <form action="/" method="post">
-  <div>
-	<span>Name: </span>
-	<?= h(isset($_SESSION) ? h($_SESSION['user_name']) : '名無しさん') ?>
+  <div class="post-nameholder">
+	<span class="oi oi-person"></span>
+	<span class="uname"><?= h(isset($_SESSION) ? h($_SESSION['user_name']) : '名無しさん') ?></span>
   </div>
   <div class="form-group">
-	<textarea cols="30" name="body" rows="10" class="form-control"></textarea>
+	<textarea cols="30" name="body" rows="5" class="form-control"></textarea>
   </div>
   <div>
 	<input name="thread_id" type="hidden" value="<?= $thread['id'] ?>"/>
